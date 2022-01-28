c858d871b72b77f953f6921ebafeb5962d44c427
Jonas Ermert <ermert.jonas@yahoo.de>
2022-01-28 11:45:24 +0100

    created function for Home and About page

--- main.go
+++ main.go
@@ -5,14 +5,28 @@ import (
 	"net/http"
 )
 
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the homepage")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the about page")
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, request *http.Request) {
-		n, err := fmt.Fprintf(w, "Go Web App gestartet")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
+
+	/*
+		http.HandleFunc("/", func(w http.ResponseWriter, request *http.Request) {
+			n, err := fmt.Fprintf(w, "Go Web App gestartet")
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+		})
+	*/
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/", About)
 
 	_ = http.ListenAndServe(":9000", nil)
 }
