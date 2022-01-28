03fa69a88a9528176af373bd423e08c83c71bac3
Jonas Ermert <ermert.jonas@yahoo.de>
2022-01-28 12:01:01 +0100

    created templates for home and about

--- main.go
+++ main.go
@@ -5,28 +5,24 @@ import (
 	"net/http"
 )
 
+const portNumber = ":9000"
+
+// Home returns the Home Page
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is the homepage")
 }
 
+// About returns the About Page
 func About(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is the about page")
 }
 
 func main() {
 
-	/*
-		http.HandleFunc("/", func(w http.ResponseWriter, request *http.Request) {
-			n, err := fmt.Fprintf(w, "Go Web App gestartet")
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-		})
-	*/
-
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/", About)
 
-	_ = http.ListenAndServe(":9000", nil)
+	fmt.Println("Starting App on port 9000...")
+	_ = http.ListenAndServe(portNumber, nil)
+
 }
